71dec923378665f0f3c988d22d51974aa9270816
GhismoV <ghismo_v@yahoo.it>
2022-09-27 22:48:15 +0200

    fine lezione filtro prodotti by nome/codArt/barcode

--- src/app/pages/articoli/articoli.component.ts
+++ src/app/pages/articoli/articoli.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { map, Observable, Observer, of, OperatorFunction } from 'rxjs';
 import { ArticoliService } from 'src/services/data/articoli.service';
 import { IArticoli } from '../../models/Articoli';
 
@@ -12,25 +14,78 @@ export class ArticoliComponent implements OnInit {
   articoli$: IArticoli[] = [];
   articoliErr: string = '';
 
+  filter$: Observable<string | null> = of('');
+  filter: string | null = '';
+
   pagina: number = 1;
   righe: number = 8;
 
-  constructor(private articoliSvc: ArticoliService) {}
+  partObs: Partial<Observer<IArticoli[]>> = {}
+
+
+  filterType : number = 0;
+
+  constructor(private articoliSvc: ArticoliService, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
-    this.articoliSvc.getArticoliByDescr('BARILLA').subscribe({
+    this.filter$ = this.route.queryParamMap.pipe(
+      map( (pm: ParamMap) => pm.get('filter') )
+    );
+
+    this.partObs = {
       next: this.handleResponse.bind(this),
       error: this.handleError.bind(this)
-    });
+    }
+
+    this.filter$.subscribe(p => this.filter  = p);
+    this.refresh();
+
   }
+
+  refresh = () : void => {
+    this.filterType = 0;
+    this.articoliErr = '';
+    if(this.filter)
+      this.getArticoli(this.filter);
+  }
+
+  getArticoli = (f : string) : void => {
+    this.filter = f;
+    this.articoli$ = [];
+    let vObs: Observable<IArticoli[]> | null = null;
+    switch(this.filterType) {
+      case 0: vObs = this.articoliSvc.getArticoloByCode(f).pipe(map(toVect)); break;
+      //case 1: vObs = this.articoliSvc.getArticoloByBarcode(f).pipe(map(item => {return [item];})); break;
+      case 1: vObs = this.articoliSvc.getArticoloByBarcode(f).pipe(map(toVect)); break;
+      case 2: vObs = this.articoliSvc.getArticoliByDescr(f); break;
+      default : break;
+    }
+    if(vObs) {
+      vObs.subscribe(this.partObs);
+    }
+  }
+
 
   handleResponse = (resp : IArticoli[]) : void => {
     this.articoli$ = resp;
+    this.filterType = 0;
   }
 
-  handleError = (err : Object) : void => {
-    this.articoliErr = err.toString();
+  handleError = (err : any) : void => {
+    if(this.filterType < 2) {
+      this.filterType++;
+      if(this.filter)
+        this.getArticoli(this.filter);
+    } else {
+      console.error(err);
+      this.articoliErr = err.error.message;
+      this.filterType = 0;
+    }
   }
 
+} // class end
 
+function toVect<T>(item: T) : T[] {
+    return [item];
 }
+
